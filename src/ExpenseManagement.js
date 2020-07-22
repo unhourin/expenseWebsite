fcebeb77eebc85c7da9cfcbae80ba4716aba8c26
username <username@example.com>
2020-07-22 11:20:00 +0900

    CSS最新版を適用/YKanta

--- src/ExpenseManagement.js
+++ src/ExpenseManagement.js
@@ -12,6 +12,7 @@ import {
 } from "react-bootstrap";
 
 import "bootstrap/dist/css/bootstrap.css";
+import "./ExpenseManagement.css";
 export default class ExpenseMangament extends React.Component {
   state = {
     DEFAULTDATA: [],
@@ -28,23 +29,14 @@ export default class ExpenseMangament extends React.Component {
   };
 
   componentDidMount() {
-    fetch("http://127.0.0.1:3001/user/list", {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        data.forEach((item) => {
-          item.isCheck = "false";
-        });
-
-        this.setState({
-          DEFAULTDATA: data,
-          ALLDATA: data,
-        });
-      });
+    let DATA = require("./data/station.json");
+    DATA.forEach((item) => {
+      item.isCheck = "false";
+    });
+    this.setState({
+      DEFAULTDATA: DATA,
+      ALLDATA: DATA,
+    });
   }
 
   runSelect = () => {
@@ -119,7 +111,7 @@ export default class ExpenseMangament extends React.Component {
   };
 
   reportApplicationMsg = () => {
-    fetch("http://127.0.0.1:3001/expense/pushMessage", {
+    fetch("http://127.0.0.1:3001/pushMessage", {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -132,187 +124,182 @@ export default class ExpenseMangament extends React.Component {
         alert(data.msg);
       });
   };
-
-  itemClick = () => {console.log("asd")};
-
+  
   render() {
     let _self = this;
     return (
       <>
-        <div style={{ marginLeft: 15 }}>
-          <Row style={{ backgroundColor: "#FFFFFF" }}>
-            <Col xs="5" style={{ backgroundColor: "##FFFFFF" }}>
-              <Row style={{ fontSize: 15 }}>
-                <Col xs="4">対象月</Col>
-                <Col xs="4">申請状態</Col>
-                <Col xs="4">確認状況</Col>
-              </Row>
+        <div className="body">
 
-              <Row style={{ fontSize: 5 }}>
-                <Col xs="4">
-                  <Dropdown>
-                    <Dropdown.Toggle
-                      variant="outline-secondary"
-                      id="dropdown-basic"
-                    >
-                      2020年7月
-                    </Dropdown.Toggle>
+          <div>
+            <p className="title">社員一覧画面</p>
+          </div>
 
-                    <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                      <Dropdown.Item href="#/action-2">
-                        Another action
-                      </Dropdown.Item>
-                      <Dropdown.Item href="#/action-3">
-                        Something else
-                      </Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </Col>
-                <Col>
-                  <Dropdown>
-                    <Dropdown.Toggle
-                      variant="outline-secondary"
-                      id="dropdown-basic"
-                    >
-                      全て
-                    </Dropdown.Toggle>
+          <div>
+            <p className="line"></p>
+          </div>
 
-                    <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-2">未申請</Dropdown.Item>
-                      <Dropdown.Item href="#/action-3">申請済み</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </Col>
-                <Col>
-                  <Dropdown>
-                    <Dropdown.Toggle
-                      variant="outline-secondary"
-                      id="dropdown-basic"
-                    >
-                      全て
-                    </Dropdown.Toggle>
+          <div className="inputGroup">
+            <Dropdown>
+              <Dropdown.Toggle
+                className = "dropDownContent"
+                variant="outline-secondary"
+                id="dropdown-basic"
+              >
+                対象月
+              </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-2">未確認</Dropdown.Item>
-                      <Dropdown.Item href="#/action-3">確認済み</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </Col>
-              </Row>
-            </Col>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
 
-            <Col xs="3" style={{ backgroundColor: "#FFFFFF" }}>
-              <Row>
-                <Col>
-                  <OverlayTrigger
-                    key={"top"}
-                    placement={"top"}
-                    overlay={
-                      <Tooltip id={`tooltip-${"top"}`}>
-                        未申請の人にメッセージを送信
-                      </Tooltip>
-                    }
-                  >
-                    <Button
-                      variant="outline-secondary"
-                      style={{ fontSize: 1 }}
-                      onClick={this.reportApplicationMsg}
-                    >
-                      メッセージ送信
-                    </Button>
-                  </OverlayTrigger>
-                </Col>
-                <Col>
-                  <OverlayTrigger
-                    key={"top"}
-                    placement={"top"}
-                    overlay={
-                      <Tooltip id={`tooltip-${"top"}`}>
-                        チェック済の請求書を印刷
-                      </Tooltip>
-                    }
-                  >
-                    <Button
-                      variant="outline-secondary"
-                      style={{ fontSize: 1 }}
-                      onClick={this.runPrint}
-                    >
-                      チェック済印刷
-                    </Button>
-                  </OverlayTrigger>
-                </Col>
-              </Row>
-            </Col>
+            <Dropdown>
+              <Dropdown.Toggle
+                className = "dropDownContent"
+                variant="outline-secondary"
+                id="dropdown-basic"
+              >
+                申請状態
+              </Dropdown.Toggle>
 
-            <Col xs="4" style={{ backgroundColor: "#FFFFFF" }}>
-              <InputGroup className="mb-3">
-                <FormControl
-                  placeholder="search station_name"
-                  onChange={this.handleSearchText}
-                />
-                <InputGroup.Append>
-                  <Button variant="outline-secondary" onClick={this.runSelect}>
-                    検索
-                  </Button>
-                </InputGroup.Append>
-              </InputGroup>
-            </Col>
-          </Row>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-2">未申請</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">申請済み</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
 
-          <Row>
-            <Table>
-              <thead></thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <input
-                      type="checkbox"
-                      onChange={_self.isAllCheck.bind(_self)}
-                      checked={this.state.isAllCheck || ""}
-                    />
-                  </td>
-                  <td>id</td>
-                  <td>username</td>
-                  <td>password</td>
-                  <td>e_mail</td>
-                  <td>birth</td>
-                  <td>employee_id</td>
-                  {/* <td>verification_token</td>
-                  <td>createdBy</td>
-                  <td>createdAt</td>
-                  <td>updatedBy</td>
-                  <td>updatedAt</td> */}
-                </tr>
-                {this.state.ALLDATA.length
-                  ? this.state.ALLDATA.map(function (item, index) {
-                      return (
-                        <tr key={index}>
-                          <td>
-                            <input
-                              type="checkbox"
-                              onChange={_self.checkThis.bind(_self, item)}
-                              checked={item.checked || ""}
-                            />
-                          </td>
-                          <td onClick={()=>{this.props.history.push("./Detailed.js")}}>{item.id}</td>
-                          <td>{item.username}</td>
-                          <td>**********</td>
-                          <td>{item.e_mail}</td>
-                          <td>{item.birth}</td>
-                          <td>{item.employee_id}</td>
-                          {/* <td>{item.verification_token}</td>
-                          <td>{item.createdBy}</td>
-                          <td>{item.createdAt}</td>
-                          <td>{item.updatedBy}</td>
-                          <td>{item.updatedAt}</td> */}
-                        </tr>
-                      );
-                    })
-                  : null}
-              </tbody>
-            </Table>
-          </Row>
+            <Dropdown>
+              <Dropdown.Toggle
+                className = "dropDownContent"
+                variant="outline-secondary"
+                id="dropdown-basic"
+              >
+                確認状態
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-2">未確認</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">確認済み</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+
+            <OverlayTrigger
+                key={"top"}
+                placement={"top"}
+                overlay={
+                  <Tooltip id={`tooltip-${"top"}`}>
+                    未申請の人にメッセージを送信
+                  </Tooltip>
+                }
+              >
+              <Button
+                className = "buttonContent"
+                variant="outline-secondary"
+                onClick={this.reportApplicationMsg}
+              >
+                メッセージ送信
+              </Button>
+            </OverlayTrigger>
+
+            <OverlayTrigger
+              key={"top"}
+              placement={"top"}
+              overlay={
+                <Tooltip id={`tooltip-${"top"}`}>
+                  チェック済の請求書を印刷
+                </Tooltip>
+              }
+            >
+              <Button
+                className = "buttonContent"
+                variant="outline-secondary"
+                onClick={this.runPrint}
+              >
+                チェック済印刷
+              </Button>
+            </OverlayTrigger>
+
+            <InputGroup className="inputTextGroup">
+              <FormControl
+                className="inputText"
+                placeholder="search station_name"
+                onChange={this.handleSearchText}
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary" onClick={this.runSelect} className="inputButton">
+                  検索
+                </Button>
+              </InputGroup.Append>
+            </InputGroup>
+
+            <div className="tableTextArea">
+              <Table>
+                <thead></thead>
+                <tbody>
+                  <tr className="trContent">
+                    <td>
+                      <input
+                        className="checkBoxContent"
+                        type="checkbox"
+                        onChange={_self.isAllCheck.bind(_self)}
+                        checked={this.state.isAllCheck || ""}
+                      />
+                    </td>
+                    <td>station_cd</td>
+                    <td>station_g_cd</td>
+                    <td>station_name</td>
+                    <td>station_name_k</td>
+                    <td>station_name_r</td>
+                    <td>line_cd</td>
+                    <td>pref_cd</td>
+                    <td>post</td>
+                    <td>lon</td>
+                    <td>lat</td>
+                    <td>open_ymd</td>
+                    <td>close_ymd</td>
+                    <td>e_status</td>
+                    <td>e_sort</td>
+                  </tr>
+                  {this.state.ALLDATA.length
+                    ? this.state.ALLDATA.map(function (item, index) {
+                        return (
+                          <tr
+                            className="trContent" 
+                            key={index}
+                          >
+                            <td>
+                              <input
+                                className="checkBoxContent"
+                                type="checkbox"
+                                onChange={_self.checkThis.bind(_self, item)}
+                                checked={item.checked || ""}
+                              />
+                            </td>
+                            <td>{item.station_cd}</td>
+                            <td>{item.station_g_cd}</td>
+                            <td>{item.station_name}</td>
+                            <td>{item.station_name_k}</td>
+                            <td>{item.station_name_r}</td>
+                            <td>{item.line_cd}</td>
+                            <td>{item.pref_cd}</td>
+                            <td>{item.post}</td>
+                            <td>{item.lon}</td>
+                            <td>{item.lat}</td>
+                            <td>{item.open_ymd}</td>
+                            <td>{item.close_ymd}</td>
+                            <td>{item.e_status}</td>
+                            <td>{item.e_sort}</td>
+                          </tr>
+                        );
+                      })
+                    : null}
+                </tbody>
+              </Table>
+            </div>
+          </div>
 
           <Row style={{ backgroundColor: "#FFFFFF" }}>もし区切りあったら</Row>
         </div>
