3ff5d7964248e172788e9a7d91381007d20b9459
unhourin <unhourin@unhourindeiMac.local>
2020-07-22 11:03:41 +0900

    更新

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import ExpenseMangament from "./ExpenseManagement";
+import Detailed from "./Detailed"
+import EP from "./employeeLsit"
 // import Demo from "./checkBox";
 // import TableDisplay from "./table";
 
 ReactDOM.render(<ExpenseMangament />, document.querySelector("#root"));
+// ReactDOM.render(<EP />, document.querySelector("#root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
